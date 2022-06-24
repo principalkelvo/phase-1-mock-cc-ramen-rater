b10c0ff5fdb5738bc2f1fb77e0df2080d2119f69
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-24 12:31:49 +0300

    commit tree c63a15bd313e12e6236468f273539dc7053d9a77

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 // write your code here
+
+let data;
+fetch(" http://localhost:3000/ramens", {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((result) => {
+    console.log("Success:", result);
+    data = result;
+    console.log("Data:", data[0].name);
+    createImage();
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+    alert("Error! Try again later \n" + error.message);
+  });
+
+function createImage() {
+  let ramenMenu = document.querySelector("#ramen-menu");
+  
+  //create image and append it to ramen menu
+  for (let i = 0; i < data.length; i++) {
+    img = document.createElement("img");
+    img.src = data[i].image;
+    ramenMenu.appendChild(img);
+  }
+
+  //adds main image 
+  let detailsImage = document.querySelector(".detail-image");
+  console.log(detailsImage.src);
+  detailsImage.src = data[0].image;
+
+  //add name to main image
+  let mainName= document.querySelector("#mainName")
+  mainName.innerHTML = data[0].name;
+
+   //add restaurant to main image
+   let restaurant= document.querySelector("#restaurant")
+   restaurant.innerHTML = data[0].restaurant;
+
+   //add rating
+   let rating = document.querySelector("#rating-display")
+   rating.innerHTML = data[0].rating;
+
+   let comment = document.querySelector("#comment-display")
+   comment.innerHTML = data[0].comment;
+}
+
+//main menu
+
+//
